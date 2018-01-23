c4f8f29589e073747c6029816d9fbcecf92e2ad5
Zekiie Irikh <zekie.irikh@gmail.com>
2018-01-23 20:15:24 +0200

    add script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-window.onload 
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+recognition.innerimResults = true;
+
+
+var p = document.createElement("p");
+var words = document.querySelector('.words');
+words.appendChild(p);
+
+console.log(recognition);
+recognition.addEventListener("result", function (e) {
+   var transcript = Array.from(e.results)
+   	   .map(function (result) {
+   	   	return result[0];
+   	   })
+   	   .map(function (result) {
+   	   	return result.transcript;
+   	   }) .join ('');
+   	   p.textContent = transcript;
+   	   if (e.result[0].isFinal) {
+   	   	p = document.createElement("p");
+   	   	words.appendChild(p);
+   	   }
+});
+
+recognition.addEventListener ('end', recognition.start);
+recognition.start();
