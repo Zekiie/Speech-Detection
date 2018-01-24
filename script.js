739913a2aebf3624e781f59260e7b61efe064618
Zekiie Irikh <zekie.irikh@gmail.com>
2018-01-24 10:50:13 +0200

    Changed bg

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 var recognition = new SpeechRecognition();
-recognition.innerimResults = true;
+recognition.interimResults = true;
 
 
 var p = document.createElement("p");
 var words = document.querySelector('.words');
 words.appendChild(p);
 
-console.log(recognition);
+
 recognition.addEventListener("result", function (e) {
    var transcript = Array.from(e.results)
    	   .map(function (result) {
@@ -17,12 +17,12 @@ recognition.addEventListener("result", function (e) {
    	   .map(function (result) {
    	   	return result.transcript;
    	   }) .join ('');
-   	   p.textContent = transcript;
-   	   if (e.result[0].isFinal) {
+    p.textContent = transcript;
+    if (e.result[0].isFinal) {
    	   	p = document.createElement("p");
    	   	words.appendChild(p);
    	   }
 });
 
-recognition.addEventListener ('end', recognition.start);
+recognition.addEventListener('end', recognition.start);
 recognition.start();
